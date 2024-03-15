4d9833d12672101c2cdaf1824eed1cce3b29506c
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-03-15 21:19:53 +0600

    responsive header section running

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <>
-      <h1 className='text-red-600 font-bold'>basic setup</h1>
+      <Header></Header>
     </>
   )
 }
