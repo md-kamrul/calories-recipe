0ed540647d8a324dce4b1a9208b40086180d46f0
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-03-15 23:46:35 +0600

    responsive hero section for small, medium and large devices done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,15 @@ const Hero = () => {
         <div className="hero h-[560px] rounded-3xl" style={{ backgroundImage: `url(${heroBg})` }}>
             <div className="hero-overlay bg-opacity-60 rounded-3xl"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="w-[897px] mb-5 text-5xl font-bold">Discover an exceptional cooking class <br />tailored for you!</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className="w-[80%] mx-auto">
+                    <h1 className="mb-5 text-5xl font-bold">
+                        Discover an exceptional cooking<br />class tailored for you!
+                    </h1>
+                    <p className="mb-5 text-lg">
+                    Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.
+                    </p>
+                    <button className="btn bg-[#0BE58A] border-2 border-[#0BE58A] hover:bg-[#0be58a65] hover:border-[#0BE58A] hover:text-white mr-6 rounded-[50px]">Explore Now</button>
+                    <button className="btn border-2 border-white bg-transparent hover:bg-transparent hover:border-[#0BE58A] text-white hover:text-[#0BE58A] mr-6 rounded-[50px]">Our Feedback</button>
                 </div>
             </div>
         </div>
