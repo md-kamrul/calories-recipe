049766fc026474e752c2d45429a86cb4e31a85c9
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-03-17 15:54:06 +0600

    waiting list running

--- src/components/SingleWaitingList.jsx
+++ src/components/SingleWaitingList.jsx
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
 
-const SingleWaitingList = ({ singleWantToCook }) => {
+const SingleWaitingList = ({ singleWantToCook, number }) => {
 
     const { recipe_name, preparing_time, calories } = singleWantToCook;
 
     return (
-        <div>
+        <div className='flex flex-row gap-5'>
 
-            <th>{singleWantToCook.length}</th>
-            <td>{recipe_name}</td>
-            <td>{preparing_time}</td>
-            <td>{calories}</td>
-            <td>
-                {/* <div className="card-actions"> */}
+            <div>{number}</div>
+            <div>{recipe_name}</div>
+            <div>{preparing_time}</div>
+            <div>{calories}</div>
+            <div>
                 <button className="btn bg-[#0BE58A] border-2 border-[#0BE58A] hover:bg-[#0be58a65] hover:border-[#0BE58A] text-[#150B2B]  rounded-[50px] font-semibold text-[12px] p-2">Preparing</button>
-                {/* </div> */}
-            </td>
+            </div>
 
         </div>
     );
--- src/components/WaitingList/WaitingList.jsx
+++ src/components/WaitingList/WaitingList.jsx
@@ -7,33 +7,33 @@ const WaitingList = ({ wantToCook }) => {
 
             <div className="text-center w-full">
                 <h1 className="text-[#282828] font-semibold text-2xl mb-6">
-                    Want To Cook: <span>00</span>
+                    Want To Cook: <span>{wantToCook.length}</span>
                 </h1>
                 <hr className="border border-[#28282833] mb-4" />
             </div>
 
             <div className="w-full">
                 <div className="text-[#878787]">
-                    <table className="table table-zebra">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Time</th>
-                                <th>Calories</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row */}
-                            <tr>
-                                {
-                                    wantToCook.map(singleWantToCook => <SingleWaitingList key={singleWantToCook.recipe_id} singleWantToCook={singleWantToCook}></SingleWaitingList>)
-                                }
-                            </tr>
-                        </tbody>
-                    </table>
+                    {/* <table className="table table-zebra"> */}
+                    {/* head */}
+                    {/* <thead> */}
+                    <div className="flex flex-row justify-between">
+                        <div></div>
+                        <div>Name</div>
+                        <div>Time</div>
+                        <div>Calories</div>
+                        <div></div>
+                    </div>
+                    {/* </thead> */}
+                    {/* <tbody> */}
+                    {/* row */}
+                    <div>
+                        {
+                            wantToCook.map(singleWantToCook => <SingleWaitingList key={singleWantToCook.recipe_id} singleWantToCook={singleWantToCook} number={wantToCook.length}></SingleWaitingList>)
+                        }
+                    </div>
+                    {/* </tbody> */}
+                    {/* </table> */}
                 </div>
             </div>
 
